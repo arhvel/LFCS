a5413ab4fa875f7c439a09e95e15cf33434d5be9
ADEYEMO, Victor Elijah <24457576+arhvel@users.noreply.github.com>
2021-08-26 18:54:19 +0100

    Add files via upload

---
+++ LFCS.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 25 16:56:58 2021
+
+@author: adeyem01
+"""
+
+import pandas as pd
+import operator, string
+from itertools import groupby
+
+class LFCS1P:
+    
+    def __init__(self, name):
+        self.name = name
+        self.data = None
+        self.sequenceDB = None
+   
+    def Data(self,path):
+        self.data = pd.read_csv(path, encoding = "ISO-8859-1")
+        self.sequenceDB= self.data.iloc[0:,1].tolist()
+        self.sdblength = len(self.sequenceDB)
+		
+    def get_SDBAvglength(self):
+        return self.sdbavglength
+
+    def get_SDBLength(self):
+        return self.summation
+        
+    def get_SDBmaxLength(self):
+        return self.sdbmaxlength
+    
+    def get_SDB(self):
+        return self.sequenceDB
+    
+    def get_SDBsize(self):
+        return self.sdblength
+      
+    
+    def lfcs(self, maxpatternlength):
+        self.userlen = maxpatternlength     
+        
+        self.minedpool = []       
+        
+
+        for i in range(len(self.sequenceDB)):
+            start = 0
+            end = self.userlen
+                
+            while (end <= len(self.sequenceDB[i])):
+                self.subpattern = (self.sequenceDB[i][start:end])
+                self.subIdx = (self.subpattern,i+1)
+                self.minedpool.append(self.subIdx)
+                start = start + 1
+                end = end + 1
+        
+        self.appearance = []
+        self.pool = sorted(self.minedpool, key=operator.itemgetter(0))
+        for pattern, indexes in groupby(self.pool, operator.itemgetter(0)):
+            subb = (pattern, [item[1:] for item in indexes])
+            _support = (len(subb[1]))
+            collate = (subb,_support)
+			
+            self.appearance.append(collate)  
+        
+        self.appear = [(x,y,z) for (x,y),z in self.appearance]
+
+        
+        #remove duplicates to find relative and absolute support
+        self.minedsequences = list(set(self.minedpool))
+
+        self.frequent = []
+
+        self.minedsequences = sorted(self.minedsequences, key=operator.itemgetter(0))
+        for pattern, indexes in groupby(self.minedsequences, operator.itemgetter(0)):
+            subb = (pattern, [item[1:] for item in indexes])
+            _support = (len(subb[1]))
+            percent = (_support/self.sdblength)*100
+            collate = (subb,_support,percent)
+			
+            self.frequent.append(collate)      
+        
+
+        self.resList = [(w,x,y,z) for (w,x),y,z in self.frequent]
+
+        
+        return self.resList, self.appear
+
+    def i_lfcs(self, maxpatternlength, startpos):
+        self.userlen = maxpatternlength     
+        
+        self.minedpool = []       
+        
+
+        for i in range(len(self.sequenceDB)):
+            start = startpos
+            end = self.userlen + startpos
+                
+            while (end <= len(self.sequenceDB[i])):
+                self.subpattern = (self.sequenceDB[i][start:end])
+                self.subIdx = (self.subpattern,i+1)
+                self.minedpool.append(self.subIdx)
+                start = start + 1
+                end = end + 1
+        
+        self.appearance = []
+        self.pool = sorted(self.minedpool, key=operator.itemgetter(0))
+        for pattern, indexes in groupby(self.pool, operator.itemgetter(0)):
+            subb = (pattern, [item[1:] for item in indexes])
+            _support = (len(subb[1]))
+            collate = (subb,_support)
+			
+            self.appearance.append(collate)  
+        
+        self.appear = [(x,y,z) for (x,y),z in self.appearance]
+
+        
+        #remove duplicates to find relative and absolute support
+        self.minedsequences = list(set(self.minedpool))
+
+        self.frequent = []
+
+        self.minedsequences = sorted(self.minedsequences, key=operator.itemgetter(0))
+        for pattern, indexes in groupby(self.minedsequences, operator.itemgetter(0)):
+            subb = (pattern, [item[1:] for item in indexes])
+            _support = (len(subb[1]))
+            percent = (_support/self.sdblength)*100
+            collate = (subb,_support,percent)
+			
+            self.frequent.append(collate)      
+        
+
+        self.resList = [(w,x,y,z) for (w,x),y,z in self.frequent]
+
+        
+        return self.resList, self.appear
+
+    
+    
+    
---
+++ LFCSstreamlit.py
@@ -0,0 +1,189 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 25 18:28:34 2021
+
+@author: adeyem01
+"""
+
+
+from LFCS import LFCS1P
+import streamlit as st
+import pandas as pd
+import base64
+
+from io import BytesIO
+
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='lCFS', index = False)
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+def to_csv(df,fname):
+        
+    return df.to_csv(fname, index = False)
+
+def get_table_download_link(df, fname):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    val = to_excel(df)
+    b64 = base64.b64encode(val)  # val looks like b'...'
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download={fname}>Download csv file</a>' # decode b'abc' => abc
+
+#df = ... # your dataframe
+#st.markdown(get_table_download_link(df), unsafe_allow_html=True)
+
+
+ResultDataFrame = pd.DataFrame()
+
+filename = None
+
+st.write("""
+         
+         # This is the l-Length Contiguous Frequent Subsequence (i.e., LCFS) Explorer.
+         ## It mines and output a user-defined length of contiguous patterns.
+         ### This data mining explorer  avails:
+         1. Provision of unique patterns repetitive frequencies.
+         2. Provision of unique patterns relative and absolute support.
+         3. Provision of unique patterns Super-sequence indexes.
+         4. Exploration and sorting of identified patterns
+         
+         
+         """)
+
+
+explorer = LFCS1P('Explorer1')
+st.write('### A LFCS instance is running.')
+    
+st.write(' ### Step 2: Do upload a two-columned csv file i.e. Index and Sequences')
+uploaded_file = st.file_uploader("Upload your csv file")
+
+
+if uploaded_file is not None:
+    st.text('Loading data...')
+    explorer.Data(uploaded_file)
+    uploadname = uploaded_file.name
+    st.write('Loading data...done!')
+    
+    
+    st.write('Enter the pattern length to be mined below:')
+    maxlength = st.number_input(label="pattern length?", min_value=1, value=3, format="%.i") 
+    
+    if st.checkbox(label='Do you want to shift the start position for mining'):
+        st.write('#### Please, ensure the start position is not longer than the maximum lenght of the sequences')   
+        startpos = st.number_input(label="Enter Start Postion?", min_value=2, value=2, format="%.i")
+        name = str(maxlength) + '-LFC patterns mined from Start Position ' + str(startpos) + ' obtained from ' + uploadname
+        filename = name
+        namee = str(maxlength) + '_'+str(startpos)+'-LFCS_' + uploadname
+        
+        ResultDataFrame = pd.DataFrame()
+        layout = st.beta_expander('Generated Patterns Panel', expanded=True)
+            
+        with layout:
+            st.write('### Step 3:')
+            if st.button(label='Run LFCS'):
+                LFCSitems, LFCSitemsAppearances = explorer.i_lfcs(maxlength,startpos)
+                
+                st.write("Mining Completed! Processing unique patterns.")
+
+        
+                # ### Secondary Operation
+                frame = pd.DataFrame(LFCSitems, columns = ['Patterns','SuperSequences','Absolute Support', 'Relative Support %'])
+                frame1 = pd.DataFrame(LFCSitemsAppearances, columns=['Patterns','SuperSequences','Appearance'])
+                frame['Appearance'] = frame1['Appearance'].copy()
+                
+                arrangement  = frame['SuperSequences']
+
+                rearranged =[]
+                for app in arrangement:
+                    converted = [item for t in app for item in t]
+                    converted.sort()
+                    rearranged.append(str(converted))
+
+
+                frame['SuperSequences'] = pd.DataFrame(rearranged,columns = ['Supersequences']) 
+               
+                #print last twenty uniqe consecutive items
+                st.write("Processing your Data Frame of uniqe contiguous items....")
+                     
+                SortedCrosstab = frame.copy()
+                        
+                SortedCrosstab = SortedCrosstab.sort_values('Absolute Support', ascending=False)
+                
+                SortedCrosstab = SortedCrosstab.reset_index(drop=True)
+                ResultDataFrame = SortedCrosstab.copy()
+                st.write("Analysis Completed ...")
+                    
+            
+                        
+                #st.write("Styling your output to fix the container below")
+                #styled = SortedCrosstab.style.set_table_styles([dict(selector='th', props=[('text-align', 'center')])])
+                #st.dataframe(styled.set_properties(**{'text-align': 'center'}).hide_index())
+                    
+                st.write('### Step 4:')
+                st.write('#### Click the link below to download ' + filename)
+                st.markdown(get_table_download_link(ResultDataFrame, namee), unsafe_allow_html=True)
+                st.write('change the downloaded file extension to .xlsx ')
+    else:    
+        name = str(maxlength) + '-LFC patterns obtained from ' + uploadname
+        filename = name
+        namee = str(maxlength) + '-LFCS_' + uploadname
+            
+        ResultDataFrame = pd.DataFrame()
+        layout = st.beta_expander('Generated Patterns Panel', expanded=True)
+        
+        with layout:
+            st.write('### Step 3:')
+            if st.button(label='Run LFCS'):
+                LFCSitems, LFCSitemsAppearances = explorer.lfcs(maxlength)
+                
+                st.write("Mining Completed! Processing unique patterns.")
+
+        
+                # ### Secondary Operation
+                frame = pd.DataFrame(LFCSitems, columns = ['Patterns','SuperSequences','Absolute Support', 'Relative Support %'])
+                frame1 = pd.DataFrame(LFCSitemsAppearances, columns=['Patterns','SuperSequences','Appearance'])
+                frame['Appearance'] = frame1['Appearance'].copy()
+
+                arrangement  = frame['SuperSequences']
+
+                rearranged =[]
+                for app in arrangement:
+                    converted = [item for t in app for item in t]
+                    converted.sort()
+                    rearranged.append(str(converted))
+
+
+                frame['SuperSequences'] = pd.DataFrame(rearranged,columns = ['Supersequences']) 
+               
+                #print last twenty uniqe consecutive items
+                st.write("Processing your Data Frame of uniqe contiguous items....")
+                     
+                SortedCrosstab = frame.copy()
+                        
+                SortedCrosstab = SortedCrosstab.sort_values('Absolute Support', ascending=False)
+                
+                SortedCrosstab = SortedCrosstab.reset_index(drop=True)
+                ResultDataFrame = SortedCrosstab.copy()
+                st.write("Analysis Completed ...")
+                    
+            
+                        
+                #st.write("Styling your output to fix the container below")
+                #styled = SortedCrosstab.style.set_table_styles([dict(selector='th', props=[('text-align', 'center')])])
+                #st.dataframe(styled.set_properties(**{'text-align': 'center'}).hide_index())
+                    
+                st.write('### Step 4:')
+                st.write('#### Click the link below to download ' + filename)
+                st.markdown(get_table_download_link(ResultDataFrame, namee), unsafe_allow_html=True)
+                st.write('change the downloaded file extension to .xlsx ')
+else:
+    st.warning("Please upload your csv file")
+
+
+            
+
